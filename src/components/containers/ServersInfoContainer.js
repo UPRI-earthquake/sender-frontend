7ddc5c5fde3b24841bfeecb9a416ddb3df351a52
chrscbs <johnchristiancabang@gmail.com>
2023-05-25 14:41:33 +0800

    feat: add toast on toggle(stream) button response

--- src/components/containers/ServersInfoContainer.js
+++ src/components/containers/ServersInfoContainer.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Button, DataTable, Column, InputSwitch, Tooltip, Panel } from 'primereact';
+import React, { useEffect, useState, useRef } from 'react';
+import { Button, DataTable, Column, InputSwitch, Tooltip, Panel, Toast } from 'primereact';
 import { default as AddServerModal } from './../modals/AddServerModal';
 import styles from "./ServersInfoContainer.module.css";
 
 import axios from 'axios';
 
 function ServersInfoContainer() {
+
+	const toast = useRef(null); //TOAST
 
 	//MODAL STATES
 	const [showAddServerModal, setAddServerModalShow] = useState(false);
@@ -33,13 +35,58 @@ function ServersInfoContainer() {
 		);
 	}
 
-	// TODO: handler for on/off (stream/stop streaming)
-	const handleToggleChange = (event, rowData) => {
-		
+	// Handler for on/off (stream/stop streaming)
+	const handleToggleChange = async (event, rowData) => {
+		try {
+			// TODO: POST request to /device/stream endpoint
+			const payload = {
+				url: rowData.url,
+				toggleValue: event.target.value
+			}
+			const streamingEndpoint = (event.target.value === true)
+				? `${process.env.REACT_APP_BACKEND_DEV}/device/stream/start`
+				: `${process.env.REACT_APP_BACKEND_DEV}/device/stream/stop`
+			
+			console.log(streamingEndpoint)
+			const response = await axios.post(streamingEndpoint, payload);
+			console.log(response)
+			
+
+			if (event.target.value) {
+				// TODO: Add Toast
+				toast.current.show({
+					severity: 'success',
+					summary: 'Slink2dali (child process) Execution Successful',
+					detail: `Device is now streaming to ${rowData.url}`,
+					life: 3000
+				});
+			} else {
+				// TODO: Add Toast
+				toast.current.show({
+					severity: 'warn',
+					summary: 'Slink2dali (child process) Exited',
+					detail: `Device stopped streaming to ${rowData.url}`,
+					life: 3000
+				});
+			}
+
+			fetchServers(); // reload servers list table
+		} catch (error) {
+			console.error(`Error handling toggle change: ${error}`);
+
+			toast.current.show({
+				severity: 'error',
+				summary: 'Error',
+				detail: 'Slink2dali (child process) Execution Failed',
+				life: 3000
+			});
+		}
 	}
 
 	return (
     <div className={styles.serversInfo}>
+			<Toast ref={toast} ></Toast>
+
 			<AddServerModal show={showAddServerModal} close={() => setAddServerModalShow(false)} onAddServer={handleAddServer}></AddServerModal>
 
 			<Panel header="Server Information">
